99bfb769d79aea2e157aec00f0b258541ee52016
Luiz Felipe Stangarlin <luizfelipestang@gmail.com>
2024-08-17 12:37:51 -0500

    export components

--- packages/embeds/js/src/index.ts
+++ packages/embeds/js/src/index.ts
@@ -3,5 +3,13 @@ export * from './features/commands'
 export type { BotProps } from './components/Bot'
 export type { PopupProps } from './features/popup/components/Popup'
 export type { BubbleProps } from './features/bubble/components/Bubble'
+export type { StandardProps } from './features/standard/components/Standard'
+
 export type * from './features/bubble/types'
 export type * from './features/popup/types'
+
+export * from './components/Bot'
+export * from './features/popup/components/Popup'
+export * from './features/bubble/components/Bubble'
+export * from './features/standard/components/Standard'
+export * from './register'
